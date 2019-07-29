de27e6e2b326ff65459ba66633dfba167ca63920
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-29 18:25:34 +0900

    Create file.h

---
+++ Matlab/InputFileGenerator/include/file.h
@@ -0,0 +1,20 @@
+#ifndef FILE_H_INCLUDED
+#define FILE_H_INCLUDED
+
+
+void
+FILE_readDataFile(void);
+
+void
+FILE_showInput(void);
+
+void
+FILE_output_data_file();
+
+void
+FILE_output_grid_bubble_file();
+
+void
+FILE_output_forcedMotion_file();
+
+#endif // FILE_H_INCLUDED
