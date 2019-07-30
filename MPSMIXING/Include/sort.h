63772379074df4be6becdfbb76e05805f3be8d64
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-30 10:35:53 +0900

    Create sort.h

---
+++ MPSMIXING/Include/sort.h
@@ -0,0 +1,20 @@
+void
+SORT_sortNeighborTable(  int  totalNumber
+						 ,int  *numberOfNeighborParticles
+						 ,int  **neighborTable
+						 ,int  order );
+
+
+void
+SORT_quickSort_inAscendingOrder(  int iParticle
+								  ,int **neighborTable
+								  ,int left
+								  ,int right );
+
+
+
+void
+SORT_quickSort_inDescendingOrder( int iParticle
+								  ,int **neighborTable
+								  ,int left
+								  ,int right );
