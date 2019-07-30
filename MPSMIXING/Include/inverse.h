4d1b9ca9affef9f0f1d594d2d680a8f2e7329c9b
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-30 10:45:00 +0900

    Create inverse.h

---
+++ MPSMIXING/Include/inverse.h
@@ -0,0 +1,17 @@
+void
+INVERSE_matrixInverse(double matrix[10][10], int size);
+
+double
+INVERSE_determinant(double matrix[10][10], int size);
+
+void
+INVERSE_setIdendityMatrix(double matrix[10][10],int size);
+
+void
+INVERSE_cofactor(double matrix[10][10], int size);
+
+void
+INVERSE_cofactorSymmetric(double matrix[10][10], int size);
+
+void
+INVERSE_transposeAndDivideDeterminant(double matrix[10][10], int size, double det);
