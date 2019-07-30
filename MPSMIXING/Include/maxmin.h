5dbee41dac87b87d9be46379c923a0ec40902362
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-30 10:44:13 +0900

    Create maxmin.h

---
+++ MPSMIXING/Include/maxmin.h
@@ -0,0 +1,42 @@
+void
+MAXMIN_setInitialMaxMinValue( int *iParticle );
+
+
+void
+MAXMIN_updateMaxMinOfParticleProperties( void );
+
+
+void
+MAXMIN_compareTheParticleWithMaxMin( int iParticle );
+
+
+void
+MAXMIN_calculateMaxAbsoluteValueOfVelocity( void );
+
+
+void
+MAXMIN_setMaxMinOfRadii( void );
+
+
+double
+MAXMIN_returnMaxRadius_ratio( void );
+
+
+double
+MAXMIN_returnMinRadius_ratio( void );
+
+
+double
+MAXMIN_returnMaxRadius( void );
+
+
+double
+MAXMIN_returnMinRadius( void );
+
+
+double
+MAXMIN_returnSmallerValue( double value1, double value2);
+
+
+void
+MAXMIN_displayMaxMinValueOfParticleProperty( void );
