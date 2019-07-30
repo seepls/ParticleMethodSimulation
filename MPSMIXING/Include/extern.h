2f6b610e351a8f71ff5d9262c43d33a3110a0eaa
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-30 10:48:00 +0900

    Create extern.h

---
+++ MPSMIXING/Include/extern.h
@@ -0,0 +1,17 @@
+int     NumberOfDimensions;
+int     FlagOfHighSpeedCalculation;
+FILE   *FpForLog;
+
+
+/*--- for cosinTransform.c ---*/
+double AverageOfSamplingData;
+double FinalTime;
+int    FlagOfWindowFunction;
+double Phase_current;
+double Amplitude_current;
+double WaveLength_current;
+double AverageWaterLevel_current;
+
+double SolutionOfLeastSquareMethod[20];
+double MinimumErrorOfApproximatedEquation;
+int    FlagOfLinearApproximation;
