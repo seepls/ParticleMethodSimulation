c212c19ec7a10a44d2b3aa324be658e496e608a3
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-30 10:49:01 +0900

    Create density.h

---
+++ MPSMIXING/Include/density.h
@@ -0,0 +1,17 @@
+void
+DENSITY_calculateParticleNumberDensity( double **position );
+
+
+void
+DENSITY_setStandardParticleNumberDensity( void );
+
+void
+DENSITY_calculateLambdaOfLaplacianOperator( void );
+
+void
+DENSITY_displayCalculatedLambda( void );
+
+
+
+void
+DENSITY_displayStandardNumberDensity( void );
