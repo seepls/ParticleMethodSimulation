591dd2f93f1abdb81783f94b058fec378902ec3c
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-07-30 10:51:56 +0900

    Create bubble.h

---
+++ MPSMIXING/Include/bubble.h
@@ -0,0 +1,26 @@
+void
+Bubble_calculateBubble(void);
+
+double
+Bubble_calculateCosine(double distanceIJ,int iParticle,int jParticle);
+
+double
+Bubble_setInfluenceRadius(void);
+
+void
+Bubble_setBetaZero(void);
+
+double
+Bubble_calculateDiameter(int iParticle);
+
+double
+Bubble_calculateVolume(double diameter);
+
+double
+Bubble_calculateRisingVelocity(double diameter);
+
+void
+Bubble_calculateBubbleRising(void);
+
+void
+Bubble_surfaceJudgement(void);
